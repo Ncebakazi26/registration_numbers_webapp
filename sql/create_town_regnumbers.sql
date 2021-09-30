568a502be43e3931bcf373ab20d79908a277ab63
Ncebakazi <ncebakazi414@gmail.com>
2021-09-30 17:16:58 +0200

    insert in the table towns

---
+++ sql/create_town_regnumbers.sql
@@ -0,0 +1,3 @@
+insert into towns (town_name, reg_string) values ('Cape Town', 'CA');
+insert into towns (town_name, reg_string) values (' Bellville ', 'CY');
+insert into towns (town_name, reg_string) values ('Paarl', 'CL');
