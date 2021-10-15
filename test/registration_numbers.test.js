da615c5e437e34c8f5b19ec833b3ce2a01425356
Ncebakazi <ncebakazi414@gmail.com>
2021-10-15 11:04:40 +0200

    style

--- test/registration_numbers.test.js
+++ test/registration_numbers.test.js
@@ -8,11 +8,14 @@ const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@l
 const pool = new Pool({
     connectionString
 });
+// eslint-disable-next-line no-undef
 describe('The regstration numbers web app', function () {
     let reg = registration_numbers(pool);
+    // eslint-disable-next-line no-undef
     beforeEach(async function () {
         await pool.query("delete from registrationnumbers");
     });
+    // eslint-disable-next-line no-undef
     it('should add the registration numbers in the db and return 1', async function () {
         await reg.setReg({
             registration_Num: 'CA 123 123'
@@ -20,6 +23,7 @@ describe('The regstration numbers web app', function () {
         let regs = await reg.getReglist()
         assert.equal(1, regs.length)
     });
+    // eslint-disable-next-line no-undef
     it('should add the registration numbers in the db and return 3', async function () {
         await reg.setReg({
             registration_Num: 'CA 123 123'
@@ -33,6 +37,7 @@ describe('The regstration numbers web app', function () {
         let regs = await reg.getReglist()
         assert.equal(3, regs.length)
     });
+    // eslint-disable-next-line no-undef
     it('should not add the same registration number in the db and must return 3 for the length', async function () {
         await reg.setReg({
             registration_Num: 'CA 123 123'
@@ -50,19 +55,23 @@ describe('The regstration numbers web app', function () {
         assert.equal(3, regs.length)
     });
 
+    // eslint-disable-next-line no-undef
     it('should return 1 for the registration number that belongs to Cape Town', async function () {
 
         assert.equal(1, await reg.getIdTown('CA'))
 
     });
+    // eslint-disable-next-line no-undef
     it('should return 2 for the registration number that belongs to Bellville', async function () {
         assert.equal(2, await reg.getIdTown('CY'))
 
     });
+    // eslint-disable-next-line no-undef
     it('should return 3 for the registration number that belongs to Paarl', async function () {
         assert.equal(3, await reg.getIdTown('CL'))
 
     });
+    // eslint-disable-next-line no-undef
     it('should display registration numbers for Cape Town if its the seclected town  ', async function () {
         await reg.setReg({
             registration_Num: "CL 854 955"
@@ -77,6 +86,7 @@ describe('The regstration numbers web app', function () {
         assert.deepEqual([{ registration_num: "CA 854 955" }], await reg.selectedTown("CA"))
 
     });
+    // eslint-disable-next-line no-undef
     it('should display registration numbers for Bellville if its the seclected town  ', async function () {
         await reg.setReg({
             registration_Num: "CL 854 955"
@@ -91,6 +101,7 @@ describe('The regstration numbers web app', function () {
         assert.deepEqual([{ registration_num: "CY 369 876" }, { registration_num: "CY 123 955" }], await reg.selectedTown("CY"))
 
     });
+    // eslint-disable-next-line no-undef
     it('should return an error saying follow the format if the digits for the registration number are morethan 6', async function () {
         await reg.setReg({
             registration_Num: "CL 123 5647"
@@ -99,6 +110,7 @@ describe('The regstration numbers web app', function () {
         assert.equal('Please follow  the format shown on the screen', await reg.getError())
 
     });
+    // eslint-disable-next-line no-undef
     it('should display the error message when the same registration number is entered ', async function () {
         await reg.setReg({
             registration_Num: "CL 123 564"
@@ -110,12 +122,14 @@ describe('The regstration numbers web app', function () {
         assert.equal('Registration number already exists', await reg.getError())
 
     });
+    // eslint-disable-next-line no-undef
     it('should delete the registration number that exist in the database', async function () {
         await reg.getReglist("CL 123 564")
 
         assert.equal("", await reg.reset())
 
     });
+    // eslint-disable-next-line no-undef
     after(function () {
         pool.end();
     })
